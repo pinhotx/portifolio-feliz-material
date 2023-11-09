ca6174cfc59407c6b5a87aaa8d8fd4d6d625c601
pinhotx <tiago.pinhotx@outlook.com>
2023-11-09 14:19:58 -0300

    ✨ Add NavDropdown, Slider Buttons and Functions

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 $(function () {
   $('.sidenav').sidenav();
+  $(".dropdown-trigger").dropdown();
+
 });
 
-$('.carousel.carousel-slider').carousel({
+ $('.carousel.carousel-slider').carousel({
   fullWidth: true,
   indicators: true
 });
+
+setSlide = function (direction = 'next') {
+  $('.carousel.carousel-slider').carousel(direction);
+}
